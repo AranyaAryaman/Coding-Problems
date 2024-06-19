0aaf9328ce73c67dd693335a75bda851bdc40416
Aranya Aryaman <39076870+AranyaAryaman@users.noreply.github.com>
2024-06-19 21:57:18 +0530

    Update MaxNonNegativeSubArray.cpp

--- MaxNonNegativeSubArray.cpp
+++ MaxNonNegativeSubArray.cpp
@@ -1,3 +1,36 @@
+/*
+Problem Description
+
+Given an array of integers, A of length N, find out the maximum sum sub-array of non negative numbers from A.
+
+The sub-array should be contiguous i.e., a sub-array created by choosing the second and fourth element and skipping the third element is invalid.
+
+Maximum sub-array is defined in terms of the sum of the elements in the sub-array.
+
+Find and return the required subarray.
+
+NOTE:  
+
+If there is a tie, then compare with segment's length and return segment which has maximum length.
+If there is still a tie, then return the segment with minimum starting index.
+
+
+Problem Constraints
+1 <= N <= 105
+
+-109 <= A[i] <= 109
+
+
+
+Input Format
+The first and the only argument of input contains an integer array A, of length N.
+
+
+
+Output Format
+Return an array of integers, that is a subarray of A that satisfies the given conditions.
+*/
+
 vector<int> Solution::maxset(vector<int> &A) {
     long long int max_sum = INT_MIN;
     long long int curr_sum = 0;
