20cdbcdd58c2c56b4f37c45e3c5f5da5a2a576e0
Aranya Aryaman <39076870+AranyaAryaman@users.noreply.github.com>
2024-09-05 01:15:53 -0400

    Update majorityElement.cpp (SEE THIS IMPLEMENTATION)

--- Greedy/majorityElement.cpp
+++ Greedy/majorityElement.cpp
@@ -45,17 +45,33 @@ Example Explanation
 2 occurs 2 times which is greater than 3/2.
 */
 
+// int Solution::majorityElement(const vector<int> &A) {
+//     int n = A.size();
+//     if(n==1 || n==2)    return A[0];
+//     unordered_map<int,int> myMap;
+//     for(int i=0;i<A.size();i++){
+//         if(myMap.find(A[i])!=myMap.end()){
+//             myMap[A[i]]++;
+//             if(myMap[A[i]]>= n/2 + 1)
+//                 return A[i];
+//         }
+//         else
+//             myMap.insert(make_pair(A[i],1));
+//     }
+// }
+
 int Solution::majorityElement(const vector<int> &A) {
     int n = A.size();
-    if(n==1 || n==2)    return A[0];
-    unordered_map<int,int> myMap;
-    for(int i=0;i<A.size();i++){
-        if(myMap.find(A[i])!=myMap.end()){
-            myMap[A[i]]++;
-            if(myMap[A[i]]>= n/2 + 1)
-                return A[i];
+    int ans = 0;
+    for(int i=0;i<32;i++){
+        int ones = 0;
+        for(int j=0;j<A.size();j++){
+            if(1<<i & A[j])
+                ones++;
         }
-        else
-            myMap.insert(make_pair(A[i],1));
+        if(ones>n/2)
+            ans+=pow(2,i);
     }
+    return ans;
 }
+
