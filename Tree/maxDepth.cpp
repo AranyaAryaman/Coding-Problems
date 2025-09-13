39b8ea04991977aa27c658059de6042481d8d871
Aranya Aryaman <39076870+AranyaAryaman@users.noreply.github.com>
2025-09-13 10:17:42 +0530

    Update maxDepth.cpp

--- Tree/maxDepth.cpp
+++ Tree/maxDepth.cpp
@@ -1,48 +1,24 @@
-/*
-Max Depth of Binary Tree
-Programming
-Tree Data Structure
-Medium
-72.6% Success
-
-227
-
-11
-
-Bookmark
-Asked In:
-Given a binary tree, find its maximum depth.
-
-The maximum depth of a binary tree is the number of nodes along the longest path from the root node down to the farthest leaf node.
-
-NOTE : The path has to end on a leaf node.
-
-Example :
-
-         1
-        /
-       2
-max depth = 2.
-*/
-
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-int Solution::maxDepth(TreeNode* A) {
-    if(A==NULL)
-        return 0;
-    else if(A && A->left==NULL && A->right==NULL)
-        return 1;
-    else if(A && A->left==NULL && A->right!=NULL)
-        return maxDepth(A->right)+1;
-    else if(A && A->left!=NULL && A->right==NULL)
-        return maxDepth(A->left)+1;
-    else
-        return 1 + max(maxDepth(A->left),maxDepth(A->right));
-}
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        else if(root && root->left==NULL && root->right==NULL)
+            return 1;
+        
+        else
+            return max(1+maxDepth(root->left),maxDepth(root->right)+1);
+
+    }
+};
