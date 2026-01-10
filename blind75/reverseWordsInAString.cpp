16709bc5905c327317d82337816140e241ae45f6
Aranya Aryaman <39076870+AranyaAryaman@users.noreply.github.com>
2026-01-11 00:01:06 +0530

    Implement reverseWords function in Solution class

---
+++ blind75/reverseWordsInAString.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<string> st;
+        string temp;
+        for(int i=0;i<s.length();i++){
+            if(s[i]==' '){
+                if(!temp.empty()){
+                    st.push(temp);
+                    temp.clear();
+                }
+                while(s[i]==' '){
+                    continue;
+                }
+            }
+            else{
+                temp += s[i];
+            }
+        }
+        if(!temp.empty())
+            st.push(temp);
+        string result;
+        while(!st.empty()){
+            result += st.top();
+            st.pop();
+            if(!st.empty())
+                result += " ";
+        }
+        return result;
+    }
+};
